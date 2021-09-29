6c23b257840f795c0ff20fa3087fa41a1680a81c
Hellomisterr <91594309+Hellomisterr@users.noreply.github.com>
2021-09-29 15:10:51 +0700

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -10,34 +10,35 @@ from .Channel import (
    GENRES
 )
 
-HELP = """
-Basic Commads
+MY_DEV = https://t.me/Ethicall_hacker
 
-/start :- check am Alive
 
-Bot Owner Only
-/broadcast Replay Any Message or Media
-/stats user Status
-/ban_user  click ban_user more info
-/unban_user click unban_user more info
-/banned_users Banned User Details
-/total How Many Files Added In Database
-/logger  Get Logs
-/delete Delete File From Database
 
-[➡️How To Add Old Files For database](https://youtu.be/mtu5hgW-l_o)
 
-@Mo_Tech_YT
-"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ABOUT = """
 ➥ Name :  Auto Filter With IMDB
     
-➥ Creator : [Muhammed RK](t.me/Mrk_YT)
+➥ Creator : [click_here](t.me/Ethicall_hacker)
 
 ➥ Language : Python3
 
 ➥ Library : Pyrogram Asyncio 1.13.0
 
-➥ Source Code : [Click Me](https://github.com/PR0FESS0R-99/LuciferMoringstar_Robot)
+➥ Source Code : [Click Me](https://t.me/joinchat/05jLQiMvnJc2Y2Y1)
 """
